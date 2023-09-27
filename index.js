8bf37101e80b5af3da34291a45459b41a82679d0
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-09-27 18:37:05 +0530

    feat: add mongo db connection

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const app = express();
 
-const PORT = 5000;
-
+// MIDDLEWARES
 app.get('/', (req, res) => res.json({ message: 'Hello world' }));
 
-app.listen(PORT, () => console.log(`App started on port ${PORT}`));
+async function initServer() {
+  const { MONGO_DB_URL, PORT } = process.env;
+
+  await mongoose.connect(MONGO_DB_URL);
+
+  console.log('Successfully connected to database');
+
+  app.listen(PORT, () =>
+    console.log(`Http Server started successfully on port ${PORT} 😎`)
+  );
+}
+
+try {
+  initServer();
+} catch (error) {
+  console.error('Could not start the server 💥', error);
+}
